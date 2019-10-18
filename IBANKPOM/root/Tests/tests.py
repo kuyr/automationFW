3837894633f6eef6372b5d57c32ae7e4db503c25
Emmanuel Kuye <emmanuel.kuye@gtbanklab.com>
2019-10-18 12:03:53 +0100

    gitignore update

--- IBANKPOM/root/Tests/tests.py
+++ IBANKPOM/root/Tests/tests.py
@@ -37,57 +37,50 @@ class TestSuite(unittest.TestCase):
 
 
 
-    # def test_02_dashboard(self):
-    #     driver = self.driver
-    #     dashboard = DashBoardPage(driver)
-    #     self.driver.implicitly_wait(5)
-    #     self.assertIn("Dashboard", self.driver.title)
-    #     self.assertIn("Frequent Transaction", self.driver.page_source)
-    #     self.assertIn("Frequent Transfers", self.driver.page_source)
-    #     self.assertIn("Frequent Topup", self.driver.page_source)
-    #     self.assertIn("Frequent Bills", self.driver.page_source)
-    #     self.assertIn("GT TrackIt", self.driver.page_source)
-    #     dashboard.view_statement_check()
-    #     time.sleep(5)
-    #     self.assertIn("View Statement", self.driver.title)
-    #     self.assertIn("Account Statement", driver.page_source)
-    #
-    #     print('Dashboard Check successful')
-    #
-    # def test_03_accounts(self):
-    #     driver = self.driver
-    #     account = AccountPage(driver)
-    #
-    #
-    #
-    #
-    #
-    #
-    #
-    #     account.send_to_3rd_party_check()
-    #     self.assertIn("Generate Statement", self.driver.title)
-    #     account.send_to_3rd_party_check()
-    #     driver.find_element_by_xpath("//span[contains(text(),'United Kingdom')]").click()
-    #     driver.find_element_by_xpath("//div[@id='aa01fee5fafe-0']//p[1]").click()
-    #
-    #     time.sleep(5)
-    #     print('Generate Statement Check successful')
-    #
-    # def test_04_account_open_dom_act(self):
-    #     driver = self.driver
-    #     account = AccountModulePage(driver)
-    #     account.open_dorm_acct_check()
-    #     time.sleep(5)
-    #     self.assertIn("Open Dom Account", self.driver.title)
-    #     print('Open Dom Account Check successful')
-    #
-    # def test_05_account_open_additional_act(self):
-    #     driver = self.driver
-    #     account = AccountModulePage(driver)
-    #     account.open_additional_acct_check()
-    #     time.sleep(5)
-    #     self.assertIn("Add Additional Account", self.driver.title)
-    #     print('Open Additional Account Successful')
+    def test_02_dashboard(self):
+        driver = self.driver
+        dashboard = DashBoardPage(driver)
+        self.driver.implicitly_wait(5)
+        self.assertIn("Dashboard", self.driver.title)
+        self.assertIn("Frequent Transaction", self.driver.page_source)
+        self.assertIn("Frequent Transfers", self.driver.page_source)
+        self.assertIn("Frequent Topup", self.driver.page_source)
+        self.assertIn("Frequent Bills", self.driver.page_source)
+        self.assertIn("GT TrackIt", self.driver.page_source)
+        dashboard.view_statement_check()
+        time.sleep(5)
+        self.assertIn("View Statement", self.driver.title)
+        self.assertIn("Account Statement", driver.page_source)
+
+        print('Dashboard Check successful')
+
+    def test_03_accounts(self):
+        driver = self.driver
+        account = AccountPage(driver)
+        account.send_to_3rd_party_check()
+        self.assertIn("Generate Statement", self.driver.title)
+        account.send_to_3rd_party_check()
+        driver.find_element_by_xpath("//span[contains(text(),'United Kingdom')]").click()
+        driver.find_element_by_xpath("//div[@id='aa01fee5fafe-0']//p[1]").click()
+
+        time.sleep(5)
+        print('Generate Statement Check successful')
+
+    def test_04_account_open_dom_act(self):
+        driver = self.driver
+        account = AccountPage(driver)
+        account.open_dorm_acct_check()
+        time.sleep(5)
+        self.assertIn("Open Dom Account", self.driver.title)
+        print('Open Dom Account Check successful')
+
+    def test_05_account_open_additional_act(self):
+        driver = self.driver
+        account = AccountPage(driver)
+        account.open_additional_acct_check()
+        time.sleep(5)
+        self.assertIn("Add Additional Account", self.driver.title)
+        print('Open Additional Account Successful')
 
 if __name__ == "__main__":
     unittest.main()
